f61685c9e7c8a0a061041ddef9cf58019df6d46a
Luis Carlin <luiscarlin@gmail.com>
2024-06-25 13:03:45 -0400

    add shadcn

--- app/routes/dashboard.documents/route.tsx
+++ app/routes/dashboard.documents/route.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function DashboardDocuments() {
-  return <div className="font-sans p-4">Dashboard Documents</div>;
+  return (
+    <div className="font-sans p-4">
+      <Button onClick={() => console.log("hey you did it")}>Click me</Button>
+    </div>
+  );
 }
