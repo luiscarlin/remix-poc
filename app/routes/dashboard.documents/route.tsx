fa96303b902a47bc7e801b4c1868cd628f5ea9b9
Luis Carlin <luiscarlin@gmail.com>
2024-06-25 12:46:35 -0400

    format

---
+++ app/routes/dashboard.documents/route.tsx
@@ -0,0 +1,3 @@
+export default function DashboardDocuments() {
+  return <div className="font-sans p-4">Dashboard Documents</div>;
+}
---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-organize-imports"],
+};
+
+export default config;
